c83253b53200c27fd11aa069b7dec5eabfd57688
shuminchang <magic2313@gmail.com>
2023-09-06 10:53:10 +0800

    formatted by black

--- Python/ml.py
+++ Python/ml.py
@@ -2,5 +2,4 @@ import pandas as pd
 import numpy as np
 import xgboost as xgb
 import sklearn
-from sklearn.model_selection import StratifiedKFold,KFold
-
+from sklearn.model_selection import StratifiedKFold, KFold
